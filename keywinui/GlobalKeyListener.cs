be5427fe7f2818dcbc01e17b8dfdfbc68769cb4b
NguyenMint <truongnm2406@gmail.com>
2024-08-27 21:26:02 +0700

    exe

---
+++ keywinui/GlobalKeyListener.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keywinui
+{
+    public class GlobalKeyListener
+    {
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr _keyboardHookID = IntPtr.Zero;
+        private static IntPtr _mouseHookID = IntPtr.Zero;
+
+        private LowLevelKeyboardProc _keyboardProc;
+        private LowLevelMouseProc _mouseProc;
+
+        private Action<int> _onKeyPressedCallback;
+        private Action<string> _onMouseActionCallback;
+
+        public GlobalKeyListener(Action<int> onKeyPressedCallback, Action<string> onMouseActionCallback)
+        {
+            _onKeyPressedCallback = onKeyPressedCallback;
+            _onMouseActionCallback = onMouseActionCallback;
+
+            _keyboardProc = KeyboardHookCallback;
+            _mouseProc = MouseHookCallback;
+
+            _keyboardHookID = SetKeyboardHook(_keyboardProc);
+            _mouseHookID = SetMouseHook(_mouseProc);
+        }
+
+        private IntPtr SetKeyboardHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr SetMouseHook(LowLevelMouseProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr KeyboardHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                _onKeyPressedCallback?.Invoke(vkCode);
+            }
+            return CallNextHookEx(_keyboardHookID, nCode, wParam, lParam);
+        }
+
+        private IntPtr MouseHookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                if (wParam == (IntPtr)WM_LBUTTONDOWN)
+                {
+                    _onMouseActionCallback?.Invoke("Mouse_Left_Button_Clicked");
+                }
+                else if (wParam == (IntPtr)WM_RBUTTONDOWN)
+                {
+                    _onMouseActionCallback?.Invoke("Mouse_Right_Button_Clicked");
+                }
+            }
+            return CallNextHookEx(_mouseHookID, nCode, wParam, lParam);
+        }
+
+        public void Unhook()
+        {
+            UnhookWindowsHookEx(_keyboardHookID);
+            UnhookWindowsHookEx(_mouseHookID);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, Delegate lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WH_MOUSE_LL = 14;
+        private const int WM_LBUTTONDOWN = 0x0201;
+        private const int WM_RBUTTONDOWN = 0x0204;
+    }
+}
---
+++ keywinui/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Core; // Sử dụng Windows.UI.Core cho CoreWindow
+using Microsoft.UI.Xaml.Input;
+using Windows.System;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace keywinui
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        private GlobalKeyListener _keyListener;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            _keyListener = new GlobalKeyListener(OnKeyPressed, OnMouseAction);
+        }
+
+        private void OnKeyPressed(int vkCode)
+        {
+           VirtualKey key = (VirtualKey)vkCode;
+            string keyString = key.ToString();
+
+            UpdateLabel($"Key Pressed: {keyString} (Code: {vkCode})");
+        }
+
+        private void OnMouseAction(string action)
+        {
+            UpdateLabel(action);
+        }
+
+        private void UpdateLabel(string text)
+        {
+            KeyPressLabel.Text += $"{text}\n";
+        }
+
+        ~MainWindow()
+        {
+            _keyListener.Unhook();
+        }
+    }
+}
